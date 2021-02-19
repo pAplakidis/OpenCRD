286a5a25c3cacc4eb24a19d485f629acb7aeaf71
pAplakidis <aplakidisp@gmail.com>
2021-02-19 16:07:41 +0200

    worked on training script

--- train_cr_detection_model.py
+++ train_cr_detection_model.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import sys
 import numpy as np
+
 import cv2
 import pims
+import matplotlib.pyplot as plt
+from plt import plot
 
 import torch
 import torch.nn as nn
@@ -13,25 +16,22 @@ import torchvision.transforms as transforms
 # resolution
 W = 1280
 H = 960
+LABEL_DICT = {0: "no crossroad", 1: "crossroad"}
 
 def get_data(video_path, log_path):
   with open(log_path, "r") as log_file:
-    labels = log_file.read().split("\n")
+    labels = log_file.read().split("\n")[:-1]
     log_file.close()
   print("Log file read")
 
-  frames = []
-  cap = cv2.VideoCapture(video_path)
-  idx = 0
-
-  # TODO: try nvidia's video loader nvvl
+  # TODO: check nvidia's nvvl [ https://github.com/NVIDIA/nvvl ] [ https://github.com/mitmul/pynvvl ]
+  # make a Video Object instead of loading all frames in memory
+  # to access frames just do: frame = frames[n]
   print("Extracting frames ...")
-  # TODO: find out why it reshapes the images
-  # NOTE: to access frames just do: frame = frames[n]
-  frames = pims.Video(video_path)
+  frames = pims.Video(video_path, format="mp4")
   print("Done extracting frames")
-    
-  return frames, np.array(labels) 
+
+  return frames, np.array(labels).astype(np.int)
 
 class Net(nn.Module):
   def __init__(self):
@@ -62,15 +62,67 @@ class Net(nn.Module):
     x = self.fc3(x)
     return x
 
+def train(X_train, Y_train):
+  # TODO: batch-process the frames
+  # NOTE: since pims reads with the original shape and swaps r and b channels, use this for the frames
+  #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  #img = cv2.resize(img, (W,H))
+  model = Net()
+  
+  # TODO: check the docs for proper training script
+  loss_function = nn.NLLLoss(reduction='none')
+  optim = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0)
+  BS = 128
+  losses, accuracies = [], []
+
+  for i in (t := trange(1000)):
+    samp = np.random.randong(0, X_train_shape[0], size=(BS))
+    X = torch.tensor(X_train[samp].reshape((-1, 28*28))).float()  # TODO: check the docs, the shape might be wrong
+    Y = torch.tensor(Y_train[samp]).long()
+    model.zero_grad()
+    out = model(X)
+    cat = torch.round(out)  # TODO: in the deployment need to print out the probability of crossroad (rounded label + nonrounded value for explainability)
+    accuracy = (cat == Y).float().mean()  # TODO: this might give AttributeError (FIX IT)
+    loss = loss_function(out, Y)
+    loss = loss.mean()
+    loss.backward()
+    optim.step()
+    loss, accuracy = loss.item(), accuracy.item()
+    losses.append(loss)
+    accuracies.append(accuracy)
+    t.set_description("loss %.2f accuracy %.2f" % (loss, accuracy))
+
+  # plot losses and accuracies
+  plt.ylim(-0.1, 1.1)
+  plot(losses)
+  plot(accuracies)
+  
+  return model
+
+# TODO: evaluate the net + compute gradients in torch
+def evaluate(model):
+  pass
+
+
 if __name__ == '__main__':
   video_path = sys.argv[1]
   log_path = video_path[:-4] + ".txt"
 
   frames, labels = get_data(video_path, log_path)
-  print("Frames:")
-  print(frames[0])
-  print(frames[0].shape)
-  print("Labels:")
-  print(labels)
-  #model = Net()
+
+  # NOTE: DEBUGGING CODE/EXAMPLE FOR PROCESSING/SHOWING FRAME
+  """
+  print("Frame 300")
+  frame = frames[300]
+  frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+  frame = cv2.resize(frame, (W,H))
+  cv2.imshow('frame', frame)
+  print("Label:", LABEL_DICT[labels[300]])
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
+  """
+
+  # model = train(frames, labels)
+  # TODO: save the model in models/
+  # evaluate(model)
 
