4c430078d2de6c0845573957622885aa5cec3c3e
pAplakidis <aplakidisp@gmail.com>
2021-04-01 21:16:49 +0300

    new crossroad detection model, more accuracy and confidence

--- model.py
+++ model.py
@@ -9,12 +9,12 @@ class CRDetector(nn.Module):
 
     # Convolutional Layers
     self.conv1 = nn.Conv2d(3, 16, 5)
-    #self.conv2_bn1 = nn.BatchNorm2d(16)
+    self.conv2_bn1 = nn.BatchNorm2d(16)
     self.pool = nn.MaxPool2d(2, 2)
     self.conv2 = nn.Conv2d(16, 32, 5)
-    #self.conv2_bn2 = nn.BatchNorm2d(32)
+    self.conv2_bn2 = nn.BatchNorm2d(32)
     self.conv3 = nn.Conv2d(32, 64, 5)
-    #self.conv2_bn3 = nn.BatchNorm2d(64)
+    self.conv2_bn3 = nn.BatchNorm2d(64)
 
     # Fully connected layers
     self.fc1 = nn.Linear(64 * 16 * 36, 120) # for 320x160 image 64 channels
@@ -24,12 +24,9 @@ class CRDetector(nn.Module):
     self.fc3 = nn.Linear(84, 1)
 
   def forward(self, x):
-    #x = self.pool(F.relu(self.conv2_bn1(self.conv1(x))))
-    #x = self.pool(F.relu(self.conv2_bn2(self.conv2(x))))
-    #x = self.pool(F.relu(self.conv2_bn3(self.conv3(x))))
-    x = self.pool(F.relu(self.conv1(x)))
-    x = self.pool(F.relu(self.conv2(x)))
-    x = self.pool(F.relu(self.conv3(x)))
+    x = self.pool(F.relu(self.conv2_bn1(self.conv1(x))))
+    x = self.pool(F.relu(self.conv2_bn2(self.conv2(x))))
+    x = self.pool(F.relu(self.conv2_bn3(self.conv3(x))))
     #print(x.shape)
     x = x.view(-1, self.num_flat_features(x))
     x = F.relu(self.bn1(self.fc1(x)))
@@ -55,6 +52,7 @@ class REDetector(nn.Module):
     n_points = 4  # number of points on each line
     max_n_lines = 6 # max number of polylines in a frame
 
+    # TODO: copy new model architecture from colab
     # Convolutional Layers
     self.conv1 = nn.Conv2d(3, 16, 5)
     self.pool = nn.MaxPool2d(2, 2)
@@ -67,9 +65,8 @@ class REDetector(nn.Module):
     self.bn1 = nn.BatchNorm1d(num_features=120)
     self.fc2 = nn.Linear(120, 84)
     self.bn2 = nn.BatchNorm1d(num_features=84)
-
-    # TODO: decide the architecture for the last neuron
     self.fc3 = nn.Linear(84, n_coords*n_points*max_n_lines)
+    return x
 
   def forward(self, x):
     x = self.pool(F.relu(self.conv1(x)))
